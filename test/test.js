16c4dcea943d13af1093e37565ce84fffb3b751e
David Streid <david.streid.nyc@gmail.com>
2020-03-27 09:06:39 -0500

    Adding tests

---
+++ test/test.js
@@ -0,0 +1,35 @@
+var expect = require("chai").expect;
+const jwtInCookie = require("../index");
+
+describe("JWT-in-cookie tests", function () {
+    it("Configured instance validates payload encoded by same secret", function () {
+        const secret = "abcdefghijklmnopqrstuvwxyz1234567";
+        const timeoutDuration = "1m";
+        const payload = {"foo": "bar"};
+        jwtInCookie.configure({secret, timeoutDuration});
+
+        const req = {
+            cookies: {
+                "jic": jwtInCookie.encodePayload(payload)
+            }
+        };
+
+        expect(() => jwtInCookie.validateRequestCookie(req)).to.not.throw();
+    });
+
+    it("Payload signed w/ different secret throws error", function () {
+        const secret = "abcdefghijklmnopqrstuvwxyz1234567";
+        const payload = {"foo": "bar"};
+        jwtInCookie.configure({secret});
+        const req = {
+            cookies: {
+                "jic": jwtInCookie.encodePayload(payload)
+            }
+        };
+
+        // Re-configure
+        jwtInCookie.configure({secret: "1234567abcdefghijklmnopqrstuvwxyz"});
+
+        expect(() => jwtInCookie.validateRequestCookie(req)).to.throw();
+    });
+});
